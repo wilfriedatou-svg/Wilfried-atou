9e352cc4638ea6f4e19d3561c7febe5ef6df6a65
Wilfried <wilfriedatou@gmail.com>
2026-07-20 03:33:47 +0100

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-{
-  "name": "wilfried-atou",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "node-telegram-bot-api": "^0.66.0"
-  }
-}
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express();
+
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token);
+const url = 'https://wilfried-atou.onrender.com';
+
+// On dit à Telegram d'envoyer les messages ici
+bot.setWebHook(`${url}/bot${token}`);
+
+app.use(express.json());
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Quand quelqu'un tape /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'HELLO, WORLD! ✅ Ton bot marche');
+});
+
+app.get('/', (req, res) => res.send('Bot is running'));
+app.listen(10000);
